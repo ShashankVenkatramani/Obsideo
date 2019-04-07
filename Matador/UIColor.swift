ef5d33eb2c6aca3ba0a462b8418dcc1c779b4aae
Aryan Kaul <aryankaul31@gmail.com>
2019-04-07 00:25:56 -0700

    Intial UI designs

---
+++ Matador/UIColor.swift
@@ -0,0 +1,33 @@
+//
+//  UIColor.swift
+//  VegaScrollFlowLayout_Example
+//
+//  Created by Pavel Yevtukhov on 9/7/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var vegaRed: UIColor {
+        return UIColor(red:0.90, green:0.22, blue:0.21, alpha:1.0)
+    }
+    
+    static var vegaGreen: UIColor {
+        return UIColor(red:0.30, green:0.69, blue:0.31, alpha:1.0)
+    }
+    
+    static var vegaGray: UIColor {
+        return UIColor(red:0.51, green:0.51, blue:0.55, alpha:1.0)
+    }
+    
+    static var vegaBlack: UIColor {
+        return UIColor(red:0.12, green:0.12, blue:0.12, alpha:1.0)
+    }
+    static var lightGreenGrad: UIColor{
+        return UIColor(red: 72.0/255.0, green: 205.0/255.0, blue: 189.0/255.0, alpha: 1.0)
+    }
+    static var darkGreenGrad: UIColor{
+        return UIColor(red: 35.0/255.0, green: 223.0/225.0, blue: 163.0/255.0, alpha: 1.0)
+    }
+}
