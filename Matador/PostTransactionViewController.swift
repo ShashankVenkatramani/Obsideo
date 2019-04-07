1a7eaf5ee44f883d3bc4d733bc453f8530c976ba
ShashankVenkatramani <shashankvxp@gmail.com>
2019-04-07 00:39:36 -0700

    Added transaction posting.

--- Matador/PostTransactionViewController.swift
+++ Matador/PostTransactionViewController.swift
@@ -8,15 +8,22 @@
 
 import UIKit
 import LocalAuthentication
+import Firebase
+import FirebaseDatabase
 class PostTransactionViewController: UIViewController {
     var uid:String = ""
+    @IBOutlet weak var targetField: UITextField!
+    @IBOutlet weak var amountField: UITextField!
     @IBAction func sendPressed(_ sender: Any) {
         let context:LAContext = LAContext()
         
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil){
             context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Authorize payment") { (pass, error) in
                 if pass {
-                    print("Accepted")
+                    let transaction = self.uid + self.targetField.text! + self.amountField.text!
+                    let database = Database.database().reference()
+                    
+                    database.child("request")
                 } else {
                     print("Failed")
                 }
