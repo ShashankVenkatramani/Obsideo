9a356f7c51cdc3e861d4328266ec4cdcdf7d8499
ShashankVenkatramani <shashankvxp@gmail.com>
2019-04-06 19:10:35 -0700

    Firebase working

--- Matador/AppDelegate.swift
+++ Matador/AppDelegate.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 import CoreData
-
+import Firebase
+import FirebaseDatabase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        var ref:DatabaseReference = Database.database().reference()
+        ref.child("memes").setValue("memes")
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-Matador/Pods-Matador-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Matador : NSObject
+@end
+@implementation PodsDummy_Pods_Matador
+@end
